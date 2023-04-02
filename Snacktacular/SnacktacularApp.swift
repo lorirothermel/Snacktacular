73f05fc8750774aa39f4dda807b281a45f1f4f21
LBRothermel <128320877+LBRothermel@users.noreply.github.com>
2023-04-02 16:31:45 -0400

    Initial Commit

---
+++ Snacktacular/SnacktacularApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacktacularApp.swift
+//  Snacktacular
+//
+//  Created by Lori Rothermel on 4/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SnacktacularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
